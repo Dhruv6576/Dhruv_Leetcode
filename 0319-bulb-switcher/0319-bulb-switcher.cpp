402b87f0e03065aa6dbfdc5fcb2118202b97c1bd
Dhruv6576 <dhruvbhalala07@gmail.com>
2026-08-15 18:44:38 +0530

    Time: 0 ms (100%), Space: 7.7 MB (69.47%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        if(n==0) return 0;
+        int count = 0;
+        int i=1;
+        while(i*i<=n){
+            count++;
+            i++;
+        }
+        return count;
+    }
+};
